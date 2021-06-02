e1c5cb1d1a2e84a327beb00c32be016d96d5c28f
Sara Ferreira <p.s.alwaysandforever@gmail.com>
2021-06-02 15:59:36 -0300

    Corrigindo o exercício 08

--- oito.java
+++ oito.java
@@ -8,17 +8,16 @@ public class oito {
 		// Data: 02/06/2021
 		
 		Scanner entrada = new Scanner(System.in);
-		int a = 29, b = 47;
-		double c, d, e, f;
+
+		double a, b, c;
 		
 		System.out.println("Informe o custo de fábrica: ");
-		c = entrada.nextDouble();
+		a = entrada.nextDouble();
 		
-		d = c+ (c*a/ 100);
-		e = c + (c*b/ 100);
-		f = c + d + e;
+		c = a + (a*0.28);
+		b = c + (c*0.45);
 		
-		System.out.println("O custo ao consumidor é: " + f);
+		System.out.println("O custo ao consumidor é: " + b);
 
 		
 	}
